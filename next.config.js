fc449998672227d7930c8b4e3ee7e753349d9507
MartijnMesman <martijn.mesman@inholland.nl>
2025-07-06 23:49:21 +0200

    Deploy to Netlify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  devIndicators: false
+  devIndicators: false,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
